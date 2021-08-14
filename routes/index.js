98ce3426ac359194c1e6c0c7ca68f637451c1347
Kygan <kygandao23@gmail.com>
2021-08-14 11:18:36 -0700

    added api route and tested

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-//ROUTES
+//INITIAL API AND HTML ROUTES
 const apiRoutes = require('./apiRoutes');
+const htmlRoutes = require('./htmlRoutes')
 
 router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
 
 module.exports = router;
