dd6a5253fd57214b907a6396b0d698a29523118e
Kygan <kygandao23@gmail.com>
2021-08-14 10:50:39 -0700

    corrected connection

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const router = express.Router();
 
+//ROUTES
+const apiRoutes = require('./apiRoutes');
+
+router.use('/api', apiRoutes);
 
 module.exports = router;
